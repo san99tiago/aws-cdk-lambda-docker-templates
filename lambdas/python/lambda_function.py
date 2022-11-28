21afe844474fec4db4dafb10ed531a59a96355a0
Santiago Garcia Arango <santiago.garcia1999@hotmail.com>
2022-11-27 23:27:03 -0500

    Update lambda python files with sample S3 script

--- lambdas/python/lambda_function.py
+++ lambdas/python/lambda_function.py
@@ -1,12 +1,34 @@
-# Sample lambda function
+# SAMPLE PYTHON LAMBDA FUNCTION FOR DOCKER IMAGE
+# Santiago Garcia Arango
 
+# Built-in imports
 import boto3
+import json
+
+
+s3_client = boto3.client("s3")
 
 
 def handler(event, context):
     print("Event: ", event)
     print("Context: ", context)
+
+    try:
+        response = s3_client.list_buckets()
+        buckets = []
+        for bucket in response["Buckets"]:
+            buckets.append(bucket["Name"])
+        return_message = {
+            "buckets": buckets
+        }
+    except Exception as e:
+        return_message = {
+            "message": "Error on the S3 list buckets operation {}".format(e)
+        }
+
+    print(json.dumps(return_message, indent=2, default=str))
+
     return {
         "statusCode": 200,
-        "body": "Sample response"
+        "body": json.dumps(return_message, indent=2, default=str)
     }
